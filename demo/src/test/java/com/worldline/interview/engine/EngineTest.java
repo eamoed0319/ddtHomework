2bdfa35419d22e5e38f53fc6a85aabccfdf2bd14
SunnyHuang <@Eamoed1903>
2022-01-30 16:25:33 +0800

    worldline home work 1.2.0

---
+++ demo/src/test/java/com/worldline/interview/engine/EngineTest.java
@@ -0,0 +1,36 @@
+package com.worldline.interview.engine;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EngineTest {
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void stop() {
+    }
+
+    @Test
+    void isRunning() {
+    }
+
+    @Test
+    void fill() {
+    }
+
+    @Test
+    void checkFuelType() {
+    }
+
+    @Test
+    void getCostPerBatch() {
+    }
+
+    @Test
+    void getBatchSize() {
+    }
+}
